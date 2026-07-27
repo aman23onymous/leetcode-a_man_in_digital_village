9fd0e401abddfdcd8591c66d622dbd0f1a3e1c3d
aman23onymous <157746957+aman23onymous@users.noreply.github.com>
2026-07-27 12:06:01 +0530

    Time: 1172 ms (45.41%) | Memory: 316 MB (55.05%) - LeetSync

---
+++ 2633-minimum-cost-to-split-an-array/minimum-cost-to-split-an-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minCost(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> dp(n + 1, 2e9);
+        dp[n] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            unordered_map<int,int> freq;
+            int trim = 0;
+
+            for (int j = i; j < n; j++) {
+                freq[nums[j]]++;
+
+                if (freq[nums[j]] == 2)
+                    trim += 2;
+                else if (freq[nums[j]] > 2)
+                    trim++;
+
+                dp[i] = min(dp[i], k + trim + dp[j + 1]);
+            }
+        }
+
+        return dp[0];
+    }
+};
