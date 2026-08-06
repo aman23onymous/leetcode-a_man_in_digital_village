f914b8d4d3b5da1d35522adbdca9221a1382cbea
aman23onymous <157746957+aman23onymous@users.noreply.github.com>
2026-08-06 20:24:27 +0530

    Time: 2198 ms (5.06%) | Memory: 218.7 MB (11.48%) - LeetSync

---
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+map<TreeNode*,bool>mp;
+int ret;
+    int sumi(TreeNode* root){
+        int su=root->val;
+        cout<<root->val<<" "<<mp[root]<<"\n";
+
+        if(root->left) su+=(sumi(root->left));
+        if(root->right) su+=(sumi(root->right));
+        if(mp[root])ret=max(ret,su);
+        return su;
+    }
+    vector<int> sde(TreeNode* root){
+        int tr=1,mx=root->val,mn=root->val;
+        if(root->left){
+            vector<int> anl=sde(root->left);
+            if(anl[0]==0) tr=0;
+            mx=max(mx,anl[1]);
+            mn=min(mn,anl[2]);
+            if(root->val<=anl[1]) tr=0;
+        }
+        if(root->right){
+            vector<int> anr=sde(root->right);
+            if(anr[0]==0) tr=0;
+            mx=max(mx,anr[1]);
+            mn=min(mn,anr[2]);
+            if(root->val>=anr[2]) tr=0;
+        }
+        mp[root]=tr;
+        return {tr,mx,mn};
+    }
+    int maxSumBST(TreeNode* root) {
+        vector<int> sd=sde(root);
+        int k=sumi(root);
+        return ret;
+    }
+};
