bf79022ddccda7fe00c54bc330e0151d8260ac7e
aman23onymous <157746957+aman23onymous@users.noreply.github.com>
2026-08-19 15:34:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (6.80%) - LeetSync

---
+++ 233-number-of-digit-one/number-of-digit-one.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[11][11][11];
+    int cnt(int i,int t,int z,string s){
+        if(i==s.size()) return z;
+        if(1+dp[i][t][z]) return dp[i][t][z];
+        int mni=9;
+        if(t) mni=s[i]-'0';
+        int ans=0;
+        for(int j=0;j<=mni;j++){
+            if(t and j==s[i]-'0') {ans+=(cnt(i+1,t,z+(j==1),s));}
+            else {
+                ans+=(cnt(i+1,0,z+(j==1),s));
+                // if(i==1)cout<<j<<ans<<" ";
+            }
+        }
+        // if(i==0) cout<<ans<<" ";
+        return dp[i][t][z]=ans;
+    }
+    int countDigitOne(int n) {
+        memset(dp,-1,sizeof(dp));
+        return cnt(0,1,0,to_string(n));
+    }
+};
