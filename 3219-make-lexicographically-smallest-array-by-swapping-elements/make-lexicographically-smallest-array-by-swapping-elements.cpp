26487178fe87a256daed115a177b281d55e62c9e
aman23onymous <157746957+aman23onymous@users.noreply.github.com>
2026-08-29 11:58:35 +0530

    Time: 131 ms (87.89%) | Memory: 149.3 MB (82.82%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-        vector<vector<int>> srt(nums.size(),vector<int>(2));
+        vector<pair<int,int>> an;
         for(int i=0;i<nums.size();i++){
-            srt[i][0]=nums[i];
-            srt[i][1]=i;
+            an.push_back({nums[i],i});
         }
-        sort(srt.begin(),srt.end());
-        int i=0;
-        while(i<nums.size()-1){
-            // cout<<i<<"\n";
-            int j=i;
-            while(j<nums.size()-1 and srt[j+1][0]-srt[j][0]<=limit) j++;
-            // cout<<i<<" "<<j<<"\n";
-            vector<int> l;
-            for(int k=i;k<=j;k++) l.push_back(srt[k][1]);
-            sort(l.begin(),l.end());
-            for(int u:l) nums[u]=srt[i++][0];
-            // for(int o=0;o<nums.size();o++) cout<<nums[o]<<" ";
-            // cout<<"\n";
-            i=j+1;
+        sort(begin(an),end(an));
+        for(int i=0;i<nums.size();i++){
+            int j=i+1;
+            vector<int> ps;
+            ps.push_back(an[i].second);
+            while(j<nums.size() and an[j].first-an[j-1].first<=limit){
+                ps.push_back(an[j].second);
+                j++;
+            }
+            sort(begin(ps),end(ps));
+            for(int k=i;k<j;k++){
+                nums[ps[k-i]]=an[k].first;
+            }
+            i=j-1;
         }
         return nums;
     }
