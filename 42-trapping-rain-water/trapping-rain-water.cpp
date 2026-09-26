f930b590213d95950a0bfc454f95b9c839f719a3
aman23onymous <157746957+aman23onymous@users.noreply.github.com>
2026-09-26 22:08:01 +0530

    Time: 87 ms (4.64%) | Memory: 27.1 MB (31.85%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int trap(vector<int>& ht) {
+        stack<int> st;
+        int ans=0;
+        for(int i=0;i<ht.size();i++){
+            while(st.size() and ht[st.top()]<ht[i]){
+                int h=ht[st.top()];st.pop();
+                if(st.size()==0)break;
+                ans+=((min(ht[st.top()],ht[i])-h)*(i-st.top()-1));
+                cout<<i<<" "<<(min(ht[st.top()],ht[i]))<<"\n";
+                // st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
